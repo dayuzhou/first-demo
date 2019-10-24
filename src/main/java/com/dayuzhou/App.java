595cfc40c912d445f5ca77f8ef19a132d0aa6855
dayuzhou <2507356905@qq.com>
2019-10-24 11:47:27 +0800

    修改部分数值

--- src/main/java/com/dayuzhou/App.java
+++ src/main/java/com/dayuzhou/App.java
@@ -14,10 +14,10 @@ public class App {
         do {
             Person person = new Person();
             person.setName("测试" + index);
-            person.setAge(20 + index * 2);
+            person.setAge(18 + index * 2);
             index++;
             System.out.println(person.toString());
 
-        } while (index < 10);
+        } while (index < 6);
     }
 }
